89c1529c484ab56085f3b524f8ba0c9069243187
obk <bahadir@kundak.dev>
2024-10-05 04:32:30 -0700

    powershell config change

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,10 @@
-# set PowerShell to UTF-8
-[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
-
-Import-Module posh-git
-$omp_config = Join-Path $PSScriptRoot ".\material.omp.json"
+$omp_config = Join-Path $PSScriptRoot ".\obk.omp.json"
 oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
 
 
-Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -PredictionViewStyle ListView
+function scf {
+    scoop update
+    scoop update *
+    scoop cleanup *
+    scoop cache rm *
+}
--- powershell/profile.ps1
+++
@@ -1,8 +0,0 @@
-
-#region conda initialize
-# !! Contents within this block are managed by 'conda init' !!
-If (Test-Path "C:\Users\bkund\miniconda3\Scripts\conda.exe") {
-    (& "C:\Users\bkund\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
-}
-#endregion
-
